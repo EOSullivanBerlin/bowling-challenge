aa534d5f1955cad034d8ed2aa851b69c207ddf5c
EOSullivanBerlin <esully666@gmail.com>
2017-08-07 10:07:41 +0200

    A game with out bonuses can be played

--- jasmine-standalone-2/spec/featureSpec.js
+++ jasmine-standalone-2/spec/featureSpec.js
@@ -1,7 +1,36 @@
 describe('Bowling Game', function() {
-  var game = new BowlingGame;
-  it('allows the player to roll a ball and get a random score between 0 - 10', function(){
-    expect(game.roll()).toBeLessThan(11);
-    expect(game.roll()).toBeGreaterThan(-1);
+  var game;
+  beforeEach(function() {
+    game = new BowlingGame;
   });
+
+  it('allows the player to add the pins they have knoked to the score', function(){
+    game.roll(10)
+    expect(game._rolls).toEqual([10]);
+  });
+
+  it('allows the player to add many rolls to the score', function() {
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    expect(game._rolls).toEqual([10, 10, 10]);
+  });
+
+  function rollMany (n, pins) {
+		for (var i = 0; i < n; i++) {
+			game.roll(pins)
+		}
+	}
+
+  it('can handle a gutter game', function() {
+    rollMany(20, 0)
+    expect(game.score()).toEqual(0)
+  });
+
+  it('can handle a game of ones', function() {
+    rollMany(20, 1)
+    expect(game.score()).toEqual(20)
+  });
+
+
 });
--- jasmine-standalone-2/src/bowlingGame.js
+++ jasmine-standalone-2/src/bowlingGame.js
@@ -1,7 +1,30 @@
 var BowlingGame = function() {
-  this._possibleScore = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  this._rolls = [];
+  this.currentRoll = 0;
 };
 
-BowlingGame.prototype.roll = function() {
-return this._possibleScore[Math.floor(Math.random() * this._possibleScore.length)];
+BowlingGame.prototype.roll = function(pins) {
+this._rolls[this.currentRoll] = pins;
+this.currentRoll += 1;
+};
+
+BowlingGame.prototype._rolls = function() {
+  return this._rolls;
+};
+
+BowlingGame.prototype.score = function() {
+  var score = 0;
+  var frameIndex = 0;
+  var game = this._rolls
+
+  function sumOfAllBallsInTheFrame() {
+		return game[frameIndex] + game[frameIndex + 1];
+  }
+
+  for (var frame = 0; frame < 10; frame++){
+    score += sumOfAllBallsInTheFrame();
+    frameIndex += 2;
+
+  }
+  return score;
 };
