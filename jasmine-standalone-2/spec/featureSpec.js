2139d2bfdcd69e5f632887a35f48345e7c84b5ea
EOSullivanBerlin <esully666@gmail.com>
2017-08-09 09:18:06 +0200

    All rules completed

--- jasmine-standalone-2/spec/featureSpec.js
+++ jasmine-standalone-2/spec/featureSpec.js
@@ -22,6 +22,15 @@ describe('Bowling Game', function() {
 		}
 	}
 
+  function rollSpare (pins) {
+    game.roll(pins)
+    game.roll(pins)
+  }
+
+  function rollStrike() {
+    game.roll(10)
+  }
+
   it('can handle a gutter game', function() {
     rollMany(20, 0)
     expect(game.score()).toEqual(0)
@@ -30,6 +39,26 @@ describe('Bowling Game', function() {
   it('can handle a game of ones', function() {
     rollMany(20, 1)
     expect(game.score()).toEqual(20)
+  });
+
+  it('can handle a sigle spare', function() {
+    rollSpare(5)
+    game.roll(3)
+    rollMany(17, 0)
+    expect(game.score()).toEqual(16)
+  });
+
+  it('can handle a single strike', function() {
+    rollStrike()
+    game.roll(1)
+    game.roll(1)
+    rollMany(16, 0)
+    expect(game.score()).toEqual(14)
+  });
+
+  it('can handle a perfect game', function() {
+    rollMany(12, 10)
+    expect(game.score()).toEqual(300)
   });
 
 
--- jasmine-standalone-2/src/bowlingGame.js
+++ jasmine-standalone-2/src/bowlingGame.js
@@ -17,14 +17,37 @@ BowlingGame.prototype.score = function() {
   var frameIndex = 0;
   var game = this._rolls
 
+  function isStrike() {
+    return game[frameIndex] === 10
+  }
+
+  function StrikeBonus() {
+    return game[frameIndex + 1] + game[frameIndex + 2]
+  }
+
+  function isASpare() {
+    return game[frameIndex] + game[frameIndex + 1] === 10
+  }
+
+  function SpareBonus() {
+    return game[frameIndex + 2]
+  }
+
   function sumOfAllBallsInTheFrame() {
 		return game[frameIndex] + game[frameIndex + 1];
   }
 
   for (var frame = 0; frame < 10; frame++){
-    score += sumOfAllBallsInTheFrame();
-    frameIndex += 2;
-
+    if(isStrike()) {
+      score += 10 + StrikeBonus();
+      frameIndex ++;
+    } else if(isASpare()) {
+      score += 10 + SpareBonus();
+      frameIndex += 2;
+    }else {
+      score += sumOfAllBallsInTheFrame();
+      frameIndex += 2;
+    }
   }
   return score;
 };
